c106afb42d0eff36b4ccf28c91dcd80818c06284
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-12-12 13:38:24 +0100

    Update Ejercicio 2.py

---
+++ Ejercicio 2.py
@@ -0,0 +1,22 @@
+def tabla_mult_fichero_comp():
+    """Función que pide un número entero entre 1 y 10 y guarde en un fichero con el nombre 
+    tabla-n.txt la tabla de multiplicar de ese número
+    
+    Parametros:
+        -Numero: un numero entero, entre 1 y 10
+        
+    Salida:
+        - Fichero con la tabla de multiplicar del numero"""
+    
+    num = int(input("INtroduce un numero entre 1 y 10"))
+
+    nombre_fichero = "Tabla-" + str(num) + ".txt" #La varieable tiene q ser un string
+
+    with open(nombre_fichero, "w") as fichero:
+        for x in range(1,11):
+
+            fichero.write(str(num) + " * " + str(x) + "\n")  #La varieable tiene q ser un string
+
+    return
+
+tabla_mult_fichero_comp()
