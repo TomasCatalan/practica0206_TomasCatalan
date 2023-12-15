cfd9671b2d88041ae40591b7c18df9d10480f9dd
TomasCatalan <tcatalaota@educacion.navarra.es>
2023-12-15 11:26:54 +0100

    Update Ejercicio 4.py

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -2,8 +2,16 @@ import urllib.request
 
 link = "http://textfiles.com/adventure/aencounter.txt"
 
-x = str(urllib.request.urlopen(link).read())
-y = x.split()
+x = urllib.request.urlopen(link)
+j = x.read()
+y = j.split()
 z = len(y)
 
 print("El numero de palabras del fichero es", z)
+
+'''
+file = urllib.request.urlopen(link)
+text= file.read()
+palabras = text.split()
+print(len(palabras))
+'''
